f50f24f1079ea171e46a087ec5efc5fc8444df3a
Aidan T. Manning <periodicaidan@gmail.com>
2020-12-02 18:35:52 -0500

    Added a blurb in the hero banner

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,7 +6,14 @@ export default function Hero(_props) {
         <div id="banner" className="hero hero-lg bg-primary">
             <div className="hero-body">
                 <h1 className="text-center">Aidan T. Manning</h1>
-                <p className="text-center text-uppercase text-light">An Overview</p>
+                <p className="text-center text-large" style={{ textShadow: '0 0 0.3em black' }}>
+                    Software engineer with a chemistry background. <br />
+                    Analytical-minded, solution-oriented, evidence-driven. <br />
+                    Proficient in front- and back-end web technologies. <br />
+                    Enthusastic about Rust and low-level/systems programming. <br />
+                    Almost a decade of technical writing experience. <br />
+                    Life is an optimization problem.
+                </p>
             </div>
         </div>
     );
