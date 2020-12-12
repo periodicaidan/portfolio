1aff62a78d0e9a440dd0a5dbe3f5e44c2bd5cb6d
Aidan T. Manning <periodicaidan@gmail.com>
2020-12-11 19:56:16 -0500

    Added footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import Navbar from './components/Navbar.svelte';
+  import Footer from './components/Footer.svelte';
 </script>
 
 <Navbar />
+<Footer />
 
 <style global>
   @import '../assets/spectre.css/dist/spectre.min.css';
---
+++ src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<footer class="hero hero-sm bg-secondary">
+  <div class="hero-body">
+    <p class="text-center">
+      Copyright &copy; 2020 Aidan T. Manning under the <a href="https://github.com/periodicaidan/portfolio/blob/Svelte/LICENSE">MIT License</a>. 
+      <br>
+      This website and its <a href="https://github.com/periodicaidan/portfolio/tree/Svelte">source code</a> are free to use.
+    </p>
+  </div>
+</footer>
